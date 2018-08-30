09fc9096dfa4075658efb80d7ebf6cc64fc16f85
KickTheKurt <42822729+KickTheKurt@users.noreply.github.com>
2018-08-30 10:36:25 +1000

    Add files via upload

---
+++ xcom.py
@@ -0,0 +1,22 @@
+import random
+
+class Player():
+  def __init__(x, y, inventory):
+    self.x = x
+    self.y = y
+    self.inventory = inventory
+    
+class Block():
+	def __init__(x, y, type):
+		self.x = x
+		self.y = y
+		self.inventory = inventory
+
+for y in range(10):
+  for x in range(40):
+
+  	symbol = '-'
+
+  	print(symbol, end='')
+
+  print('')
